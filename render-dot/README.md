72771ec7a0552077ae1afd314c055a8e8b9306aa
Sebastián Gurín <sgurin@awalabs.com>
2019-06-08 11:20:37 -0300

    render-dot

--- render-dot/README.md
+++ render-dot/README.md
@@ -4,8 +4,35 @@ Renders [Graphviz](http://www.graphviz.org) .dot files to svg, png, and other fo
 
 Based on Graphviz [Emscripten](http://kripken.github.io/emscripten-site/ port [viz.js](https://github.com/mdaines/viz.js) library.
 
- * Supports node.js and browser JavaScript API
- * CLI
+ * Supports node.js and browser JavaScript API.
+ * CLI.
+ * Outputs png, svg, json, dotx, jpeg. 
+
+## Usage
+
+### Install
+
+```sh
+npm install render-dot
+```
+
+### JavaScript
+
+```ts
+import {renderDot} from 'render-dot'
+
+const svgCode = await renderDot({
+  code: 'digraph { a -> b; }'
+})
+```
+
+### Command Line
+
+TODO
+
+## Options
+
+See [types.ts](src/types.ts). Options apply both to JavaScript API and CLI.
 
 ## Status
 
--- render-dot/src/types.ts
+++ render-dot/src/types.ts
@@ -1,13 +1,39 @@
-export interface Options extends VizOptions {
+export interface Options {
+  /**
+   * .dot code to render.
+   */
   code: string;
-}
-interface VizOptions {
+
+  /**
+   * Output image width. 
+   */
   width?: number;
+
+  /**
+   * 
+   */
   height?: number;
+
+  /**
+   * Output format. By default 'svg'
+   */
   format?: 'svg' | 'png-image-element' | 'json' | 'xdot' | 'plain' | 'ps';
+
+  /**
+   * Graph render engine. By default 'dot.
+   */
   engine?: 'dot' | 'circo' | 'fdp' | 'neato' | 'asage' | 'twopi';
+
+  /**
+   * Format of output image.
+   */
   mimeType?: 'image/jpeg' | 'image/png';
+  /** 
+   * Quality of output image.
+   */
   quality?: number;
-  scale?: boolean;
-  nop?: number;
+
+  // scale?: boolean;
+
+  // nop?: number;
 }
