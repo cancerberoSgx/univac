33bfb9759ed7e429f6e72a96d61b88e64b5ebd0e
Sebastián Gurín <sgurin@awalabs.com>
2019-06-08 17:04:48 -0300

    svg raster example

--- render-dot/test/probes.ts
+++ render-dot/test/probes.ts
@@ -1,25 +1,5 @@
 import { writeFileSync, readFile } from 'fs';
 
-// var  jsdom = require("jsdom").jsdom;
-
-// jsdom().documentElement
-// // var body = d3.select(jsdom().documentElement).select("body");
-
-// var width = 1000,
-//     height = 500;
-
-// var svg = body.append("svg");
-
-// var svg = body.append("svg")
-//     .attr("version", "1.1")
-//     .attr("xmlns", d3.namespaces.svg)
-//     .attr("xmlns:xlink", d3.namespaces.xlink)
-//     .attr("width", width)
-//     .attr("height", height)
-//     .attr("viewBox", "0 0 " + width + " " + height);
-
-// process.stdout.write(body.node().innerHTML);
-
 
 
 import {fabric, } from 'fabric'
@@ -60,7 +40,6 @@ export const fabricImageToUrl = promisify (
 
 import { decode, encode } from "universal-base64";
 // import { prom } from "misc-utils-of-mine-generic";
-async function fabricTest2() {
 const svg = `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="300" height="150" viewBox="0 0 300 150" xml:space="preserve">
@@ -71,12 +50,29 @@ const svg = `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 <circle style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255,0,0); fill-rule: nonzero; opacity: 1;"  cx="0" cy="0" r="50" />
 </g>
 </svg>`
+async function fabricTest2() {
 
   var canvas = new fabric.Canvas('c')
-  canvas.add( await fabricImageToUrl('data:image/svg;base64,'+encode(svg), {}))
+  canvas.add( await fabricImageToUrl('data:image/svg+xml;base64,'+encode(svg), {}))
   let s = ''
 s = canvas.toDataURL({format: 'jpeg'})
 var base64Data = s.substring(s.indexOf(';base64,')+';base64,'.length)
 writeFileSync("tmp2.jpeg", base64Data, 'base64')
 }
-fabricTest2()
+// fabricTest2()
+
+async function fabricTest3() { 
+  
+    var canvas = new fabric.Canvas('c')
+    canvas.add( await fabricImageToUrl('data:image/svg+xml;base64,'+encode(svg), {}))
+    let s = ''
+  s = canvas.toDataURL({format: 'jpeg'})
+  console.log(s);
+  
+  // var base64Data = s.substring(s.indexOf(';base64,')+';base64,'.length)
+  const i = document.createElement('img')
+  i.src = s
+  document.body.append(i)
+  // writeFileSync("tmp2.jpeg", base64Data, 'base64')
+  }
+  fabricTest3()
