f2ad479c6b435242d198a7fcb598d01150e50837
Sebastián Gurín <sgurin@awalabs.com>
2019-06-08 20:41:17 -0300

    svg-png

--- svg-png/src/potrace.ts
+++ svg-png/src/potrace.ts
@@ -1,22 +1,30 @@
 import { promisify } from 'util';
 var potrace = require('potrace');
+
 export const potraceTrace = promisify(potrace.trace as (url: string, o: PotraceTraceOptions, callback: (err: NodeJS.ErrnoException | null, svg: string) => void) => void);
+
 export const potracePosterize = promisify(potrace.posterize as (url: string, o: PotracePosterizeOptions, callback: (err: NodeJS.ErrnoException | null, svg: string) => void) => void);
-interface PotracePosterizeOptions extends PotraceTraceOptions {
+
+export interface PotracePosterizeOptions extends PotraceTraceOptions {
   /**
-   *  Number of samples that needs to be taken (and number of layers in SVG). (default: Posterizer.STEPS_AUTO, which most likely will result in 3, sometimes 4). Threshold computation for more than 5 levels may take a long time
+   *  Number of samples that needs to be taken (and number of layers in SVG). (default: Posterizer.STEPS_AUTO,
+   *  which most likely will result in 3, sometimes 4). Threshold computation for more than 5 levels may take
+   *  a long time
    */
   steps?: number;
   /**
-   *  How to select fill color for color ranges - equally spread or dominant. (default: PotraceFillPolicy.FILL_DOMINANT).
+   *  How to select fill color for color ranges - equally spread or dominant. (default:
+   *  PotraceFillPolicy.FILL_DOMINANT).
    */
   fill?: PotraceFillPolicy;
   /**
-   * How to choose thresholds in-between - after equal intervals or automatically balanced. (default: PotraceRangeDistributionPolicy.RANGES_AUTO).
+   * How to choose thresholds in-between - after equal intervals or automatically balanced. (default:
+   * PotraceRangeDistributionPolicy.RANGES_AUTO).
    */
   rangeDistribution?: PotraceRangeDistributionPolicy;
 }
-interface PotraceTraceOptions {
+
+export interface PotraceTraceOptions {
   /**
    * how to resolve ambiguities in path decomposition (default PotraceTurnPolicy.TURNPOLICY_MINORITY).
    */
@@ -54,25 +62,30 @@ interface PotraceTraceOptions {
    */
   background?: string;
 }
-enum Potrace {
+
+export enum Potrace {
   COLOR_AUTO = 'auto',
   COLOR_TRANSPARENT = 'transparent',
   THRESHOLD_AUTO = -1
 }
-enum Posterizer {
+
+export enum Posterizer {
   STEPS_AUTO = -1
 }
-enum PotraceRangeDistributionPolicy {
+
+export enum PotraceRangeDistributionPolicy {
   RANGES_AUTO = 'auto',
   RANGES_EQUAL = 'equal'
 }
-enum PotraceFillPolicy {
+
+export enum PotraceFillPolicy {
   FILL_SPREAD = 'spread',
   FILL_DOMINANT = 'dominant',
   FILL_MEDIAN = 'median',
   FILL_MEAN = 'mean'
 }
-enum PotraceTurnPolicy {
+
+export enum PotraceTurnPolicy {
   TURNPOLICY_BLACK = 'black',
   TURNPOLICY_WHITE = 'white',
   TURNPOLICY_LEFT = 'left',
