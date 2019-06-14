52ee24fe37c38ecb0361d7a6873312dee984e621
Sebastián Gurín <sgurin@awalabs.com>
2019-06-14 20:01:09 -0300

    graph works

--- tree-sitter-highlights-dot/src/index.ts
+++ tree-sitter-highlights-dot/src/index.ts
@@ -1,5 +1,5 @@
 import {renderDot } from 'render-dot'
-import {RemoveProperties, PropertyOptional } from 'misc-utils-of-mine-generic'
+import {RemoveProperties, PropertyOptional, notSame } from 'misc-utils-of-mine-generic'
 import { writeFileSync } from 'fs';
 
 interface GrammarNode {
@@ -9,51 +9,66 @@ interface GrammarNode {
   name?: string
   id: string
 }
-// interface Node  {
-//   text?: string
-//   // label: string,
-//   // attrs: {}
-//   id: string,
-// }
-
-// for "choice" use this: , shape=record, fixedsize=false, peripheries=1  for the parent
-// and this for thie lines: 
-// s1307:p0 -> s1306
-// s1307:p1 -> s1308
 
 let counter = 1
-
 
 export function buildGraph(n: PropertyOptional<GrammarNode, 'id'>, parent?:GrammarNode, output: string[] = []){
   setId(n);
   if(n.type==='SEQ'){
-    output.push(`${n.id} [ label="${n.id}\n&rarr; SEQ"];`);
+    // if(parent){
+    //   n.id = parent.id 
+    //   n.type = parent.id
+    // }
+    output.push(`${n.id} [ label="${n.id}\\n&rarr; SEQ"];`);
+    let lastChild: GrammarNode = n as GrammarNode
     (n.members||[]).forEach((c, i, a)=>{
-      const previous = i>0 ? a[i-1] : n
+      lastChild=c
       setId(c)
-      setId(previous)
-      output.push(`${previous.id} -> ${c.id} [];`);
-      buildGraph(c, n as any,  output)
+      buildGraph({...c, members: []}, n as any,  output)
+      if(i===0){
+        output.push(`${n.id} -> ${c.id} [];`);
+      }
+      else {
+        // buildGraph(c, n as any,  output)
+        const previous = a[i-1]
+        setId(previous)
+        const endPoints = [...buildGraph(previous, n as any,  output)]//, ...i===a.length-1 ? [] : [previous]].filter(notSame)
+        endPoints.forEach(cc=>{
+          output.push(`${cc.id} -> ${c.id} [];`)
+        })
+      }
     })
+    return [lastChild]
   }
   else if(n.type==='CHOICE'){
-    output.push(`${n.id} [label="{${n.id}\n&rarr; CHOICE|{${(n.members||[]).map((m, i)=>`<p${i}>`).join('|')}}}", shape=record, fixedsize=false, peripheries=1];`);
+    output.push(`${n.id} [label="{${n.id}\\n&rarr; CHOICE|{${(n.members||[]).map((m, i)=>`<p${i}>`).join('|')}}}", shape=record, fixedsize=false, peripheries=1];`);
+    const endPoints : GrammarNode[] = [];
     (n.members||[]).forEach((c, i, a)=>{
       setId(c)
-      buildGraph(c, n as any, output)
+      endPoints.push(...buildGraph(c, n as any, output))
       output.push(`${n.id}:p${i} -> ${c.id} [label="${getEdgeLabel(n as GrammarNode, c)}"];`);
     })
+    return endPoints
   }
   else if (n.value){
-    output.push(`${n.id} [label="${n.id}\n${n.value}"];`);
+    output.push(`${n.id} [label="${n.id} ${n.type}\\n${n.value}"];`);
+    // return [n as GrammarNode]
   }
   else if (n.type==='SYMBOL'){
-    output.push(`${n.id} [label="${n.id}\n${n.name}"];`);
+    output.push(`${n.id} [label="${n.id} ${n.type}\\n${n.name}"];`);
+    // return [n as GrammarNode]
   }
-  return output
+  else{
+    output.push(`${n.id} [label="${n.id} ${n.type}\\n${n.type} (not supported)"];`);
+  }
+  // return []
+  return [n as GrammarNode]
+
+  // return output
 }
+
 function setId(n: PropertyOptional<GrammarNode, "id">) {
-  n.id = n.id || ((counter++) + '').padStart(3, '0')
+  n.id = n.id || ('s'+((counter++) + ''))//.padStart(3, '0')
 }
 
 function getEdgeLabel(n: GrammarNode, c: GrammarNode) {
@@ -115,6 +130,18 @@ ${result.join('\n')}
 test()
 
 
+
+// interface Node  {
+//   text?: string
+//   // label: string,
+//   // attrs: {}
+//   id: string,
+// }
+
+// for "choice" use this: , shape=record, fixedsize=false, peripheries=1  for the parent
+// and this for thie lines: 
+// s1307:p0 -> s1306
+// s1307:p1 -> s1308
 
 // export function buildGraph(name: string, n: PropertyOptional<GrammarNode, 'id'>, output: GraphNode[] = []){
 //   n.id=n.id || (counter++)+''
